76dbda94154bfe5f65137e4e896fc86f8bfdff22
$.m.lnk <smjvlab@gmail.com>
2018-01-09 12:59:53 +0200

    Less2 DZ3 - multiplication table of a given number

---
+++ src/ua/jvlab/smlnk/TablRuns.java
@@ -0,0 +1,33 @@
+/*
+* This application displays the multiplication table of a given numbr number,
+* numbr is user-defined
+ */
+package ua.jvlab.smlnk;
+
+import java.util.Scanner;
+
+/**
+ * @author: $.m.lnk
+ * @version: 1.0.0
+ * @date: 09.01.2018
+ */
+public class TablRuns {
+
+    int numbr;
+
+    /**
+     * @param args the command line arguments, number is user-defined
+     */
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Введите число, таблицу умножения которого нужно вывести:");
+            int numbr = sc.nextInt();
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(numbr + " x " + i + " = " + numbr * i);
+                sc.close();
+            }
+
+        }
+
+    }
+}
